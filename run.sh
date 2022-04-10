39845bff0b2bed3431ba6a91fc72703cdf2179e1
Junbum Lee <jun@beomi.net>
2022-04-10 08:54:00 +0000

    KcELECTRA-v2022-0404-800k-discriminator

---
+++ run.sh
@@ -0,0 +1,8 @@
+python3 run_seq_cls.py --task nsmc --config_file KcELECTRA-v2022-0404-800k-discriminator.json
+python3 run_seq_cls.py --task kornli --config_file KcELECTRA-v2022-0404-800k-discriminator.json
+python3 run_seq_cls.py --task paws --config_file KcELECTRA-v2022-0404-800k-discriminator.json
+python3 run_seq_cls.py --task question-pair --config_file KcELECTRA-v2022-0404-800k-discriminator.json
+python3 run_seq_cls.py --task korsts --config_file KcELECTRA-v2022-0404-800k-discriminator.json
+python3 run_ner.py --task naver-ner --config_file KcELECTRA-v2022-0404-800k-discriminator.json
+python3 run_squad.py --task korquad --config_file KcELECTRA-v2022-0404-800k-discriminator.json
+
